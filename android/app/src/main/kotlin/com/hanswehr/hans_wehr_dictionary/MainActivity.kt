5806dbcc5bcb838387f974541b40c4ee78b91e83
GibreelAbdullah <gibreel.khan@gmail.com>
2026-05-31 18:37:37 +0530

    fixed build error

--- android/app/src/main/kotlin/com/hanswehr/hans_wehr_dictionary/MainActivity.kt
+++ android/app/src/main/kotlin/com/hanswehr/hans_wehr_dictionary/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.muslimtechnet.hanswehr
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity()
